cabad446da658ecaccb99ee1e8dd64d513989a01
Aashish Loknath Panigrahi <asxyzp@gmail.com>
2022-05-22 00:57:07 +0530

    Using Fetch API to fetch user and are data

--- src/API/getAreas.js
+++ src/API/getAreas.js
@@ -3,8 +3,15 @@
  * @description METHOD FOR GET API ENDPOINT FOR /areas
  * @returns {Promise} res 
  */
-const getAreas = () => {
-
+const getAreas = async () => {
+    try{
+        const res = await fetch("https://kyupid-api.vercel.app/api/areas");
+        return res;
+    }
+    catch(err){
+        console.error("ERROR OCCURED WHILE GETTING AREA DATA", err);
+        return null;
+    }
 };
 
 export default getAreas;
--- src/API/getUsers.js
+++ src/API/getUsers.js
@@ -3,8 +3,15 @@
  * @description METHOD FOR GET API ENDPOINT FOR /users
  * @returns {Promise} res 
  */
- const getUsers = () => {
-
+const getUsers = async () => {
+    try {
+        const res = await fetch("https://kyupid-api.vercel.app/api/users");
+        return res;
+    }
+    catch (err) {
+        console.error("ERROR OCCURED WHILE GETTING AREA DATA", err);
+        return null;
+    }
 };
 
 export default getUsers;
