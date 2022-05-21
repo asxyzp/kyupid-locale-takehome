bc9bfd10d6f0ee152fc99a75b9d1ebdc9cd10295
Aashish Loknath Panigrahi <asxyzp@gmail.com>
2022-05-21 20:09:17 +0530

    Adding Splash (screen) & ModalRouter component

---
+++ src/Components/Modals/Splash.js
@@ -0,0 +1,19 @@
+// IMPORTING PACKAGES/MODULES
+import React from 'react';
+import { Box, Typography } from '@mui/material';
+
+/**
+ * @name Splash
+ * @description SPLASH SCREEN COMPONENT
+ * @param {*} props COMPONENT PROPS
+ * @returns <Splash /> (JSX)
+ */
+const Splash = (props) => {
+    return (
+        <Box sx={{ display: "flex", justifyContent: "center", alignItems: "center", position: "absolute", bgcolor: "common.white", zIndex: "2000", height: "100%", width: "100%" }}>
+            <Typography variant="h1" sx={{ px: "8px", py: "8px", mt: "8px", borderRadius: "12.5px", fontFamily: "'Source Serif Pro', serif", fontWeight: "800", textAlign: "center", color: "common.white", bgcolor: "primary.dark" }}> Ky </Typography>
+        </Box>
+    );
+};
+
+export default Splash;
