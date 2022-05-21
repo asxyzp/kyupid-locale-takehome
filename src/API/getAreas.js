797b999f128f9376fbf9a56fa829bfefb10a5c0c
Aashish Loknath Panigrahi <asxyzp@gmail.com>
2022-05-22 00:34:55 +0530

    Adding exportable functions within getAreas and getUsers

---
+++ src/API/getAreas.js
@@ -0,0 +1,10 @@
+/**
+ * @name getAreas
+ * @description METHOD FOR GET API ENDPOINT FOR /areas
+ * @returns {Promise} res 
+ */
+const getAreas = () => {
+
+};
+
+export default getAreas;
---
+++ src/API/getUsers.js
@@ -0,0 +1,10 @@
+/**
+ * @name getUsers
+ * @description METHOD FOR GET API ENDPOINT FOR /users
+ * @returns {Promise} res 
+ */
+ const getUsers = () => {
+
+};
+
+export default getUsers;
