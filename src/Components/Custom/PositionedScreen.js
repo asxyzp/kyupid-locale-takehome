314e8335365540ac068ccba7a865f0ad6569a86d
Aashish Loknath Panigrahi <asxyzp@gmail.com>
2022-05-21 22:53:41 +0530

    Creating component for screens positioned relative to side drawer

---
+++ src/Components/Custom/PositionedScreen.js
@@ -0,0 +1,24 @@
+// IMPORTING MODULES/PACKAGES
+import React from 'react';
+import { Box } from '@mui/material';
+import { useAppContext } from '../../Context/AppContext';
+
+/**
+ * @name PositionedScreen
+ * @description POSITIONED SCREEN COMPONENT (WRT drawerWidth)
+ * @param {*} props COMPONENT PROPS
+ * @returns <PositionedScreen /> (JSX)
+ */
+const PositionedScreen = (props) => {
+
+    // GETTING APP CONTEXT
+    const { drawerWidth } = useAppContext();
+
+    return (
+        <Box sx={{ height: "100%", width: `calc(100vw - ${drawerWidth})`, marginLeft: drawerWidth, }}>
+            {props.children}
+        </Box>
+    );
+};
+
+export default PositionedScreen;
