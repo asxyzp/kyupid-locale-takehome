cb6d005f7e27ef2c4f9b6a8478633f3fa7b6c2b3
Aashish Loknath Panigrahi <asxyzp@gmail.com>
2022-05-31 08:09:02 +0530

    Updating page not found screen & adding image to it

--- src/Components/Screen/PageNotFound.js
+++ src/Components/Screen/PageNotFound.js
@@ -1,6 +1,7 @@
 // IMPORTING PACKAGES/MODULES
 import React from 'react';
-import { Box } from '@mui/material';
+import { Box, Typography } from '@mui/material';
+import PageNotFoundImg from "../../Assets/Images/PageNotFound.svg";
 
 /**
  * @name PageNotFound
@@ -10,7 +11,19 @@ import { Box } from '@mui/material';
  */
 const PageNotFound = (props) => {
     return (
-        <Box>PageNotFound</Box>
+        <Box sx={{
+            p: "10px",
+            width: "100%",
+            height: "100%",
+            display: "flex",
+            alignItems: "center",
+            flexDirection: "column",
+            justifyContent: "center",
+        }}>
+            <img src={PageNotFoundImg} className="page-not-found-img" alt="Page not found"/>
+            <Typography variant="h1" sx={{ mt: "10px", color: "primary.dark" }}>404</Typography>
+            <Typography variant="body1" sx={{ textAlign: "center" }}>Whoops! Looks like what you're searching for is missing</Typography>
+        </Box>
     );
 };
 
